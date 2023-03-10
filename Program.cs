d04e544ecb0af2fd96c1673826d24a67a1c3e832
dadaya <liola.soloviov@mail.ru>
2023-03-10 13:05:30 +0300

    stage 3

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ for (int i =0; i<5; i++)
     st[i] = Console.ReadLine();
 }
 string[] st1 = new string[5];
+int j = 0;
+for (int i=0; i<5; i++)
+{
+    if (st[i].Length  <= 3)
+    {
+        st1[j] = st[i];
+        j++;
+    }
+}
