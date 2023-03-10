c838bc81e5f242c2eb0665bb9093c714cb14c19e
dadaya <liola.soloviov@mail.ru>
2023-03-10 13:01:19 +0300

    stage 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//написать программу, которая из имеющегося массива строк сформирует массив строк, длина которых не привышает 3
+Console.WriteLine("Введите элементы массива строк: ");
+string[] st = new string[5];
+for (int i =0; i<5; i++)
+{
+    st[i] = Console.ReadLine();
+}
+string[] st1 = new string[5];
